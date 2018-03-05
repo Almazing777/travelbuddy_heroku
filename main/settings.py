a98741b983a5da3cfd08705c1b8c0c725643e947
Almas Mendygaliyev <31186194+Almazing777@users.noreply.github.com>
2018-03-05 12:42:42 -0500

    Update settings.py

--- main/settings.py
+++ main/settings.py
@@ -120,11 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 
-
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
-
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
